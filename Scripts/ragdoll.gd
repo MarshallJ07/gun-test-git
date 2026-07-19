f5496182e8328516e9650b14040cfb8aeb64fee1
MarshallJ07 <marshallejamieson@gmail.com>
2026-07-19 13:16:37 -0400

    Update ragdoll.gd

--- Scripts/ragdoll.gd
+++ Scripts/ragdoll.gd
@@ -7,11 +7,15 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
-		update_cam.rpc()
+		update_cam.rpc_id(id)
 		
 @rpc("authority","call_local","reliable")
 func update_cam() -> void:
 	get_parent().get_parent().get_node("players").get_node(str(id)).global_position = global_position
 	if linear_velocity.length() < 0.5 and $Timer.is_stopped():
-		get_parent().get_parent().get_node("players").get_node(str(id)).show()
-		queue_free()
+		delete_ragdoll.rpc()
+		
+@rpc("authority","call_local","reliable")
+func delete_ragdoll() -> void:
+	get_parent().get_parent().get_node("players").get_node(str(id)).show()
+	queue_free()
